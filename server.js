9ad2b90a11d063a83cf086eb3e9c6e65d3c03dca
Mathew <mathewharvey@gmail.com>
2020-11-05 20:52:24 +0800

    Js files updating. Got json push working.

--- server.js
+++ server.js
@@ -1,60 +1,58 @@
-
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-const { PRIORITY_BELOW_NORMAL } = require("constants");
+// Dependencies
+// =============================================================
+const express = require("express");
 
 
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-let id = 1
-
-class note {
-  constructor(title, text) {
-    this.id = id++;
-    this.title = title;
-    this.text = text;
-    this.create = new Date().toUTCString();
-  }
-}
-const notes = [
-  new note("My first ever note", 'hello world'),
-  new note('my second ever note', 'goodbye world')
-]
+// Sets up the Express app to handle data parsing
+// =============================================================
+app.use("/assets", express.static("./assets"));
+app.use(express.static("public"));
 
 
-app.get("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  res.json(notes.filter(x => x.id === id));
-})
 
 
-app.post("/api/notes", function (req, res) {
-  const newNote = new note(req.body.title, req.body.text);
-  notes.push(newNote);
-  res.json(newNote);
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
 
-app.put("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  const notesIndex = notes.findIndex(x => x.id === id)
-  const newNote = new note(req.body.title, req.body.text);
-  notes[notesIndex] = newNote;
-  res.json(notes);
-})
-
-app.delete("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  const notesIndex = notes.findIndex(x => x.id === id)
-  notes.splice[notesIndex] = newNote;
-  res.json(newNote);
-})
 
 
+// var express = require("express");
+// var path = require("path");
+// var fs = require("fs");
+
+
+// var app = express();
+// var PORT = 3000;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// let id = 1
+
+// class note {
+//   constructor(title, text) {
+//     this.id = id++;
+//     this.title = title;
+//     this.text = text;
+//     this.create = new Date().toUTCString();
+//   }
+// }
+// const notes = [
+//   new note("My first ever note", 'hello world'),
+//   new note('my second ever note', 'goodbye world')
+// ]
 
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -64,16 +62,53 @@ app.delete("/api/notes/:id", function (req, res) {
 //   res.sendFile(path.join(__dirname, "./public/notes.html"));
 // });
 
-// app.get("/api/notes", function(req, res) {
-//   return res.json(db);
+// app.get("/api/notes/:id", function (req, res) {
+//   const id = req.params.id;
+//   res.json(notes.filter(x => x.id === id));
+// })
+
+
+// app.post("/api/notes", function (req, res) {
+//   const newNote = new note(req.body.title, req.body.text);
+//   notes.push(newNote);
+//   res.json(newNote);
 // });
 
+// app.put("/api/notes/:id", function (req, res) {
+//   const id = req.params.id;
+//   const notesIndex = notes.findIndex(x => x.id === id)
+//   const newNote = new note(req.body.title, req.body.text);
+//   notes[notesIndex] = newNote;
+//   res.json(notes);
+// })
+
+// app.delete("/api/notes/:id", function (req, res) {
+//   const id = req.params.id;
+//   const notesIndex = notes.findIndex(x => x.id === id)
+//   notes.splice[notesIndex] = newNote;
+//   res.json(newNote);
+// })
 
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+
+// // app.get("/", function(req, res) {
+// //   res.sendFile(path.join(__dirname, "./public/index.html"));
+// // });
+
+// // app.get("/notes", function(req, res) {
+// //   res.sendFile(path.join(__dirname, "./public/notes.html"));
+// // });
+
+// // app.get("/api/notes", function(req, res) {
+// //   return res.json(db);
+// // });
+
+
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, function () {
+//   console.log("App listening on PORT " + PORT);
+// });
 
 
